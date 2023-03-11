cb4486dfdee8cd93ff72abf8cf808951a05ccef8
Vital Ozierski <vital@ozerich.com>
2023-03-11 18:18:12 +0300

    1.3.1 - tager 1.9 support

--- src/ModuleSettings.php
+++ src/ModuleSettings.php
@@ -3,9 +3,7 @@
 namespace OZiTAG\Tager\Backend\ModuleSettings;
 
 use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Support\ServiceProvider;
 use OZiTAG\Tager\Backend\ModuleSettings\Jobs\GetSettingPublicValueJob;
-use OZiTAG\Tager\Backend\ModuleSettings\Jobs\GetSettingValueJob;
 
 class ModuleSettings
 {
@@ -14,6 +12,6 @@ class ModuleSettings
     public function getPublicValue($module, $param, $type)
     {
         $job = new GetSettingPublicValueJob($module, $param, $type);
-        return $this->dispatchNow($job);
+        return $this->dispatchSync($job);
     }
 }
